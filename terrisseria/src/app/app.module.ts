bcd5908c1213548d3ee73e0574420cb4697c268d
Maria Cabrera <mariacabrera03@gmail.com>
2024-06-17 21:46:17 -0300

    Se crearon los modulos product y cart. Se diseño la interfaz Products y el Mock para poder maquetar.

--- terrisseria/src/app/app.module.ts
+++ terrisseria/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './product-list/product-list.component';
+import { CartComponent } from './cart/cart.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent
+    ProductListComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
--- terrisseria/src/app/product-list/product-list.component.ts
+++ terrisseria/src/app/product-list/product-list.component.ts
@@ -1,16 +1,32 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Product } from './product';
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.scss'
 })
 export class ProductListComponent {
-  product = {
-    "Tipo": "Taza",
-    "Precio": "1200",
-    "Stock": "3",
-    "image": "assets/img/taza.jpg"
+  products: Product []= 
+  [{
+    image: "assets/img/taza.jpg",
+    tipo: "Taza",
+    precio: 9500,
+    stock: 1,
+    clearance:false,
+  },
+  {
+    image: "assets/img/plato.jpg",
+    tipo: "Plato",
+    precio: 12000,
+    stock: 0,
+    clearance: false,
+  },
+  {
+    image: "assets/img/compotera.jpg",
+    tipo: "Compotera",
+    precio: 8300,
+    stock: 15,
+    clearance:true,
   }
-
+  ]
 }
